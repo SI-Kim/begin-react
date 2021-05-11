2fe4b54c232296db0ac769dc8c996cff354c235f
sung-ilkim <skim88.1942@gmail.com>
2021-05-11 21:58:57 +0900

    1-14 removing on array

--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-export const User = ({ user }) => {
+export const User = ({ user, onRemove }) => {
     return (
         <div>
             <b>{user.username}</b>
             <span>({user.email})</span>
+            <button 
+                onClick={() => onRemove(user.id)}
+                >
+                삭제
+            </button>
         </div>
     );
 }
 
-export const UserList = ({ users}) => {
+export const UserList = ({ users, onRemove }) => {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} />
+                <User user={user} key={user.id} onRemove={onRemove} />
             ))}
         </div>
     );
