502b41acafc355c068ca9c0b48d16896d2ce89a1
sung-ilkim <skim88.1942@gmail.com>
2021-05-11 21:15:29 +0900

    1-10 Selecting DOM with useRef

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+    const nameInput = useRef();
 
     const { name, nickname } = inputs;
 
-
-    const onChange = (e) => {
+    const onChange = e => {
         const { value, name } = e.target;
         setInputs({
             ...inputs,
             [name]: value
         });
     };
-
     const onReset = () => {
         setInputs({
             name: '',
             nickname: ''
         });
-    };
-
+        nameInput.current.focus();
+    }
  return (
      <div>
-         <input name="name" placeholder="이름" onChange={onChange} value={name} />
+         <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
          <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
          
          <button onClick={onReset}>초기화</button>
